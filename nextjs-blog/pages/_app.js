f18f7c28d25605437a35952bf9c71c1a16d9ecee
catalinadreisig <catalinadreisig@yahoo.dk>
2024-04-24 20:05:58 +0200

    hewj

---
+++ nextjs-blog/pages/_app.js
@@ -0,0 +1,21 @@
+
+import { Poppins, Libre_Baskerville } from 'next/font/google'
+ 
+export const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
+export const libre_baskerville = Libre_Baskerville({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+
+/*export default function MyApp({ Component, pageProps }) {
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  )
+}*/
